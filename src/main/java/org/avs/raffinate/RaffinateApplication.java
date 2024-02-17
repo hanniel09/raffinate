7170f1c539de979f0a6be9cde6642ac3fd07fc6a
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-02-16 22:06:49 -0300

    initial commit

---
+++ src/main/java/org/avs/raffinate/RaffinateApplication.java
@@ -0,0 +1,13 @@
+package org.avs.raffinate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffinateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaffinateApplication.class, args);
+    }
+
+}

0fba4fc115934fce3dd82954590a2d4d70afb226
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-02-21 20:44:33 -0300

    feat: Create UserRepository

---
+++ src/main/java/org/avs/raffinate/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.avs.raffinate.repositories;
+
+import org.avs.raffinate.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}

efcf402014732218b2e1c97ea4b93282dd2f595d
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-02-23 11:39:30 -0300

    feat: create AuthenticationController

---
+++ src/main/java/org/avs/raffinate/controllers/AuthenticationController.java
@@ -0,0 +1,53 @@
+package org.avs.raffinate.controllers;
+
+import jakarta.validation.Valid;
+import org.avs.raffinate.DTO.user.AuthenticationDTO;
+import org.avs.raffinate.DTO.user.LoginResponseDTO;
+import org.avs.raffinate.DTO.user.RegisterDTO;
+import org.avs.raffinate.domain.user.User;
+import org.avs.raffinate.infra.security.TokenService;
+import org.avs.raffinate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.userRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User user = new User(data.login(), encryptedPassword, data.role());
+
+        this.userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
+}
